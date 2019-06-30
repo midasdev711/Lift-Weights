0cdeede247dab8ea26c2d981a96457e657ad88fb
midasdev711 <midasdev711@gmail.com>
2019-06-29 23:29:11 -0700

    setup front-end login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,15 +1,26 @@
 import React, {Component} from 'react';
+import { Form, FormGroup, Label, Input, Button, Col, FormFeedback } from 'reactstrap';
 import './Login.css';
 
 class Login extends Component {
     render() {
         return (
             <div className="Login">
-                <p>
-                    <input id="name" type="text" />
-                    <input id="password" type="password" />
-                    <button id="login">Login</button>
-                </p>
+                <Form>
+                    <FormGroup row>
+                        <Label for="loginUsername" id="loginLabel" sm="4">Username</Label>
+                        <Col sm="4">
+                            <Input type="username" name="username" id="loginUsername"/>
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Label for="loginPassword" id="loginLabel" sm="4" >Password</Label>
+                        <Col sm="4">
+                            <Input type="password" name="password" id="loginPassword" />
+                        </Col>
+                    </FormGroup>
+                    <Button id="submitButton">Submit</Button>
+                </Form>
             </div>
         );
     }
