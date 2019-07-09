97c61b338141d1db2b0f021b0d583668210201fc
midasdev711 <midasdev711@gmail.com>
2019-07-09 13:31:47 -0700

    add Nav and Tabs components into Profile component

---
+++ server/client/src/components/Nav/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+import './index.css';
+
+class Nav extends Component {
+    render() {
+        return (
+            <div className="Nav">
+                Nav
+            </div>
+        );
+    }
+}
+
+export default Nav;
---
+++ server/client/src/components/Tabs/index.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import { Button, Form, Grid, Message, Segment } from 'semantic-ui-react';
+
+import './index.css';
+
+class Tabs extends Component {
+    render() {
+
+        return (
+            // Theme is modified from Semantic UI React's Tabs Layout
+            // https://github.com/Semantic-Org/Semantic-UI-React/blob/master/docs/src/layouts/TabsLayout.js
+            <div className="Tabs">
+                Tabs
+            </div>
+        );
+    }
+}
+
+export default Tabs;
