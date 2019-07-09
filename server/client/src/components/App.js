f0a9039dc1f166765b728f9dcbaf75be86acdd73
midasdev711 <midasdev711@gmail.com>
2019-07-08 18:30:58 -0700

    update imports and component names

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -2,13 +2,14 @@ import React, {Component} from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Header from '../shared_components/Header';
-import Footer from '../shared_components/Footer';
+import Header from '../shared_components/Header/Header';
+import Footer from '../shared_components/Footer/Footer';
+import Avatar from './shared_components/Avatar/Avatar';
+
 import Login from './Login/Login';
 import Register from './Register/Register';
-import Profile from './Profile/Profile';
-
 import './App.css';
+
 class App extends Component {
 
     render() {
@@ -16,7 +17,7 @@ class App extends Component {
             <BrowserRouter>                
                 <div className="App">
                     <Header />
-                    <Profile />
+                    <Avatar />
                     <Route exact path="/" component={Login} />
                     <Route exact path="/register" component={Register} />
                     <Footer />
