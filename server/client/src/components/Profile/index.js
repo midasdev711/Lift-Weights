6f23ed2342b90eda748f205ed93cfe8517fadf5e
midasdev711 <midasdev711@gmail.com>
2019-07-10 22:13:46 -0700

    add links and skeleton pages #40
    adjust margins
    
    add Router details
    
    add username, increase margins, remove hover outline

--- server/client/src/components/Profile/index.js
+++ server/client/src/components/Profile/index.js
@@ -1,17 +1,33 @@
 import React, {Component} from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
 
 import Nav from '../../shared_components/Nav';
 import Tabs from '../Tabs';
+import Workouts from '../Workouts';
+import Exercises from '../Exercises';
+import Measurements from '../Measurements';
+import Settings from '../Settings';
+import Logout from '../Logout';
 
 import './index.css';
 
 class Profile extends Component {
     render() {
         return (
-            <div className="Profile" >
-                <Nav />
-                <Tabs />
-            </div>
+            <BrowserRouter>                
+                <div className="Profile" >
+                    <Nav />
+                    <Container>
+                        <Route exact path="/profile" component={Tabs} />
+                        <Route exact path="/profile/workouts" component={Workouts} />
+                        <Route exact path="/profile/exercises" component={Exercises} />
+                        <Route exact path="/profile/measurements" component={Measurements} />
+                        <Route exact path="/profile/settings" component={Settings} />
+                        <Route exact path="/logout" component={Logout} />
+                    </Container>
+                </div>
+            </BrowserRouter>                
         );
     }
 }
