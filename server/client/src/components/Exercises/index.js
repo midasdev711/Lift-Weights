8686a769135550e2aa3ec8c50cc0b5a8b6d0f718
midasdev711 <midasdev711@gmail.com>
2019-07-16 10:42:57 -0700

    add visibility update to results, add class/key to each exercise
    display exercise images, center align
    
    update deadlines

--- server/client/src/components/Exercises/index.js
+++ server/client/src/components/Exercises/index.js
@@ -7,30 +7,45 @@ import ExerciseList from '../../shared_components/ExerciseList';
 import './index.css';
 
 class Exercises extends Component {
-    state = { exercises: [] };
+    state = { 
+        exercises: [],
+        visible: false,
+        foundStatement: <p id="Found"></p>,
+        exerciseList: <div></div>
+    };
 
     onSearch = async search => {
         const response = await wger.get('/exercise/search', {
             params: { term: search }, 
         });
-        this.setState({ exercises: response.data.suggestions });
+        await this.setState({ exercises: response.data.suggestions, visible: true });
+        await this.updateVisibility();
+        console.log('set visibility state to: ' + this.state.visible);
+    };
+
+    updateVisibility = () => {
+        if (this.state.visible === true) {
+            this.setState({ foundStatement: <p id="Found">Found: {this.state.exercises.length} exercises</p>,
+                            exerciseList: <ExerciseList exercises={this.state.exercises} />
+            });
+        }
     };
 
     render() {
         return (
             <div className='Exercises'>
-                <Grid className='Grid' textAlign='center'>
-                    <Grid.Column className='Column' rows={3}>
-                        <Grid.Row className='RowSearch'>
+                <Grid className='Grid' textAlign='center' divided='vertically'>
+                    <Grid.Row className='Row'>
+                        <Grid.Column className='Column'>
                             <Search onSearch={this.onSearch} />
-                        </Grid.Row>
-                        <Grid.Row className='RowFound'>
-                            Found: {this.state.exercises.length} exercises
-                        </Grid.Row>
-                        <Grid.Row className='RowList'>
-                            <ExerciseList exercises={this.state.exercises} />
-                        </Grid.Row>
-                    </Grid.Column>
+                            {this.state.foundStatement}
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row className='Row' columns={1}>
+                        <Grid.Column className='Column'>
+                            {this.state.exerciseList}
+                        </Grid.Column>
+                    </Grid.Row>
                 </Grid>
             </div>
         );
--- server/client/src/shared_components/ExerciseList/index.js
+++ server/client/src/shared_components/ExerciseList/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { Segment, List, Image } from 'semantic-ui-react';
 import './index.css';
 
 
 const ExerciseList = props => {
     const exerciseList = props.exercises.map((exercise) => {
-        return <div>{exercise.data.name}</div>
+        return (
+            <List.Item className="ExerciseItem" key={exercise.data.id}>
+                <Image className="ExerciseImage" src={exercise.data.image} verticalAlign="middle" />
+                <List.Content>
+                    <List.Header>{exercise.data.name}</List.Header>
+                    <p>{exercise.data.category}</p>
+                </List.Content>
+            </List.Item>
+        );
     });
 
-    return <div>{exerciseList}</div>;
+    return (
+        <div className="ExerciseList">
+            <Image.Group size="mini">
+                <Segment>
+                    <List divided relaxed>
+                        {exerciseList}
+                    </List>
+                </Segment>
+            </Image.Group>
+        </div>
+    );
 }
 
 export default ExerciseList;
